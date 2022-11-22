46db02e9dfb8399073a4f7445c8c35b49bdf086b
malrang <ruehd7924@gmail.com>
2022-11-22 18:57:58 +0900

    ✏️[chore]: 코드베이스 UI를 위한 앱 초기 세팅 #1

---
+++ iOS-malrangMarket(ReactorKit)/iOS-malrangMarket(ReactorKit)/Source/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iOS-malrangMarket(ReactorKit)
+//
+//  Created by 김동욱 on 2022/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
